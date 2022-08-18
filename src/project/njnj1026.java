74c9d8d87f80edda12ffc5122ad5ac38ed23bb5c
njnj1 <njnj1@najin>
2022-08-18 14:52:44 +0900

    njnj1026

---
+++ src/project/njnj1026.java
@@ -0,0 +1,19 @@
+package project;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class njnj1026 extends Application{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
